bfa32b9a20aba5fcd4ec079950d14b9c4dbdc0ab
kakumanu-gayatri <gayatri.kakumanu@gmail.com>
2020-04-04 16:49:47 -0400

    Code Review Task 1

--- apps/stocks/src/environments/environment.ts
+++ apps/stocks/src/environments/environment.ts
@@ -6,8 +6,8 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: false,
-  apiKey: '',
-  apiURL: 'https://sandbox.iexapis.com'
+  apiKey: 'pk_6b429fdbb1e1471e90093980c259ccab',
+  apiURL: 'https://cloud.iexapis.com'
 };
 
 /*
