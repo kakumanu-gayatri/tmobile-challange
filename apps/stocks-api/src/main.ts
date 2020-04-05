571449d341a5272cfab8da6776d1d5610858247b
kakumanu-gayatri <gayatri.kakumanu@gmail.com>
2020-04-05 19:36:40 -0400

    Task 4

--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -3,6 +3,8 @@
  * This is only a minimal backend to get started.
  **/
 import { Server } from 'hapi';
+const wreck = require('@hapi/wreck');
+const crypto = require('crypto');
 
 const init = async () => {
   const server = new Server({
@@ -10,13 +12,30 @@ const init = async () => {
     host: 'localhost'
   });
 
+  const getStockQuotes = async (request, h) => {
+    console.log('request', request.url, request.symbol);
+    const { url, symbol, period, token } = request;
+    const { response, payload } = await wreck.get(`${url}/beta/stock/${symbol}/chart/${period}?token=${token}`);
+    console.log('response', response, payload);
+    // get sha1 hash of stringified response payload to use as Etag
+    const hash = crypto.createHash('sha1');
+    hash.update(JSON.stringify(payload));
+    const etag = hash.digest('base64');
+    // setting etag to response
+    return h.response(payload).etag(etag);
+  };
+
   server.route({
     method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return {
-        hello: 'world'
-      };
+    path: '/beta/stock/{symbol}/chart/{period}',
+    handler: async (request, h) => {
+      return await getStockQuotes(request, h);
+    },
+    options: {
+      cache: {
+        privacy: 'private',
+        expiresIn: 10 * 100000
+      }
     }
   });
 
